51aa25abd14b281dad3155fcab3cae5c31e06390
Jikun Li <jikun.li@icloud.com>
2020-01-30 17:51:45 -0500

    improved comments

--- couple-three.cc
+++ couple-three.cc
@@ -1,8 +1,8 @@
-// Calculates the hopping Hamiltonian matrix of a system where a particle "e" with angular momentum Se 
-// hops between two identical sites "a" and "b", each site has angular momentum Ss.
-// S is the total angular momentum with many possible values, but only calculate the ones provided by the user.
-// All angular momentum are integers or half integers, therefore twice values (2Ss, 2Se, 2S, etc.) are stored as int.
-//
+/* Calculates the hopping Hamiltonian matrix of a system where a particle "e" with angular momentum Se 
+ * hops between two identical sites "a" and "b", each site has angular momentum Ss.
+ * S is the total angular momentum with many possible values, but only calculate the ones provided by the user.
+ * All angular momentum are integers or half integers, therefore twice values (2Ss, 2Se, 2S, etc.) are stored as int.
+*/
 // To compile: g++ --std=c++11 couple-three.cc -lgsl -lgslcblas -lm -fopenmp -o couple-three
 
 #include <cstdio>
@@ -25,8 +25,6 @@ int main(int argc, char* argv[]) {
     int twoSs, twoSe, temp_2S;
     // Handles input. Need at least 3 integers.
     // Format: ./couple-three 2Sa 2Se 2S1 2S2 2S3 ...
-    // Todo: Allow input without S1, S2, etc. (calculate for all possible S by default)
-    // Todo: Allow input format like Slow-Shigh, with intervals of 2. Need to check parity.
     try {
         if (argc < 4) throw 1;
         twoSs = std::stoi(argv[1]);
@@ -38,9 +36,9 @@ int main(int argc, char* argv[]) {
             // 0 <= S <= 2Ss+Se and 2S should have the same parity with 2Se.
             if ( temp_2S > twoSs * 2 + twoSe || temp_2S < 0 || (temp_2S & 1) != (twoSe & 1) ) throw 3;
             // Uses a map to provide reverse lookup from an 2S value to its index in twoS_values.
-            // If an S is listed more than once, inserts after the 1st time will be skipped silently.
             std::pair<std::map<int,int>::iterator, bool> try_insert;
             try_insert = twoS_index.insert({temp_2S, i-3});
+            // If an S is listed more than once, later inserts into the map will fail and be skipped silently.
             if (try_insert.second) {
                 twoS_values.push_back(temp_2S);
                 // Each S adds 2S+1 states.
@@ -48,6 +46,7 @@ int main(int argc, char* argv[]) {
             }
         }
     }
+    // Error message.
     catch (...) {
         fprintf(stderr, "Erroneous parameters found. Correct usage: ./couple-three 2Ss 2Se 2S1 [2S2 2S3 ...]\n\n");
         fprintf(stderr, "At least 3 non-negative integer parameters needed satisfying the rules of angular momentum coupling.\n");
@@ -61,23 +60,23 @@ int main(int argc, char* argv[]) {
     
     // Calculates each matrix element.
     //printf("2Ma, 2Mb, 2Me, 2Sinta, 2Sintb, 2Sa, 2Sb, i, j, mat_elem\n");
+    /* OpenMP to parallelize the first 3 levels of loop,
+     * a total of (2Ss+1)*(2Ss+1)*(2Se+1) tasks that can be done in parallel.
+     *  Not trying further because a lot of private variables besides loop indices. */
     int twoMa, twoMb, twoMe;
     #pragma omp parallel for collapse(3) private(twoMa, twoMb, twoMe) shared(t_matrix)
     for (twoMa = -twoSs; twoMa <= twoSs; twoMa +=2) {
         for (twoMb = -twoSs; twoMb <= twoSs; twoMb +=2) {
             for (twoMe = -twoSe; twoMe <= twoSe; twoMe +=2) {
-                //#ifdef _OPENMP
-                //if (twoMa == twoSs && twoMb == twoSs & twoMe == twoSe)
-                //    printf("Ran with %d threads.\n", omp_get_num_threads());
-                //#endif
                 // Total M.
                 int twoM = twoMa + twoMb + twoMe;
                 // Sa & Se couple into |Sinta, Ma+Me> state
                 // Sb & Se couple into |Sintb, Mb+Me> state
                 for (int twoSinta = std::max(abs(twoMa+twoMe), abs(twoSs-twoSe)); twoSinta <= twoSs+twoSe; twoSinta +=2) {
                     for (int twoSintb = std::max(abs(twoMb+twoMe), abs(twoSs-twoSe)); twoSintb <= twoSs+twoSe; twoSintb +=2) {
-                        // Iterates over possible S values to see if the two total states, |Sa, M> and |Sb, M>,
-                        // are possible. If yes, calculates the contribution to matrix element <Sa, M| T |Sb, M>
+                        /* Iterates over possible S values to see if the two total states, |Sa, M> and |Sb, M>,
+                         * are possible. If yes, calculates the contribution by <Ss,Ma,Se,Me,Ss,Mb|T|Ss,Ma',Se,Me',Ss,Mb'>
+                         * to the matrix element <Sa, M| T |Sb, M'> in the couple states basis. */
                         for (auto&& twoSa : twoS_values ) {
                             for (auto&& twoSb : twoS_values) {
                                 if (twoSa >= abs(twoM) && twoSa >= abs(twoSinta-twoSs) && twoSa <= twoSinta+twoSs
@@ -88,6 +87,7 @@ int main(int argc, char* argv[]) {
                                         * cg_coef(twoSs, twoMa, twoSe, twoMe, twoSinta, twoMa+twoMe)
                                         * cg_coef(twoSintb, twoMb+twoMe, twoSs, twoMa, twoSb, twoM)
                                         * cg_coef(twoSs, twoMb, twoSe, twoMe, twoSintb, twoMb+twoMe);
+                                    // Will lead to race condition if multiple threads try to update t_matrix.
                                     #pragma omp atomic
                                     t_matrix[i][j] += mat_elem_temp;
                                     //printf("%d %d %d %d %d %d %d %d %d %f\n", twoMa, twoMb, twoMe, twoSinta, twoSintb, twoSa, twoSb, i, j, mat_elem_temp);
@@ -109,6 +109,7 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
+
 
 // Clebsch-Gordan coefficients, wrapper for the GSL 3j-symbol function.
 double cg_coef(int two_j1, int two_m1, int two_j2, int two_m2, int two_j3, int two_m3) {
